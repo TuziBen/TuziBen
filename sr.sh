46b41162b39555522d3fe382c8052dd09392b7ce
TuziBen <pengxingxing@gmail.com>
2018-05-28 16:30:12 +0800

    第一行代码
    父与子结对编程

---
+++ sr.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+/hu ,/
+/
+‘、hh';l';';;;';'
+rv;;r'
+
+'ex  就；看毛利率；’；‘;l;l;kk';lllmmmmlllk;l;kmn,knnx
+d
+d\d
+asX\'
+s、
+D\'d\
+    DC\
+    dd
+dsdx
+- jjkhklhjhkllok,\l]D@d2fb8gKoriUA0525 bggktg5
